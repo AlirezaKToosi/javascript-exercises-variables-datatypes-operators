57bce139f93b7e9469dc52f163d262b652a1b3ed
Alireza KafshdarToosi <a.k.toosi@gmail.com>
2024-01-10 14:45:17 +0100

    added new exercises

---
+++ index.js
@@ -0,0 +1,35 @@
+//console.log("Hello World!")
+//************************************************************ */
+//let age=38;
+//console.log(`I am ${age} years old`);
+//************************************************************ */
+let Firstname="Alireza";
+let age=38;
+let male=true;
+console.log(`I am ${Firstname} and i am ${age} years old`);
+console.log(`Male = ${male}`);
+//************************************************************* */
+const name="Alireza";
+//name=reza;
+/************************************************************** */
+let number1=10;
+let number2=15;
+let result=number1+number2;
+let result1=number2-number1;
+let result2=number1*number2;
+let result3=number2/number1;
+console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+let a=5;
+console.log(a);
+a+=5;
+console.log(a);
+/************************************************************** */
+let a2 = 10;
+a2 = a2 - 4;
+a2 = 7;
+a2 = a2 + 2;
+console.log(a2);
+/***************************************************************** */
